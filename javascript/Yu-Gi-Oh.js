bdea2f4601e408921d6ca8fe745cbee6757a65d3
GreyGooseVX <total.egal1234@web.de>
2020-12-21 20:03:17 +0100

    banned list converter added

--- javascript/Yu-Gi-Oh.js
+++ javascript/Yu-Gi-Oh.js
@@ -1,30 +1,37 @@
-function convertButton() {
+const sneakConfHeaders = "#[2SneakCardPool]\n!2SneakCardPool\n$whitelist\n"
+const preXYZConfHeaders = "#[1PreXYZ_Custom]\n!1PreXYZ_Custom\n$whitelist\n"
+let cardpoolMap = new Map()
+let convertedYdk = ""
+
+function sneakPeekConverterButton() {
 	let ydk = document.getElementById("ydk").value
-	const confHeaders = "#[2SneakCardPool]\n!2SneakCardPool\n$whitelist\n"
 	ydk = ydk.replace(/#main|#extra|!side/gi, "")
 	ydk = ydk.replace(/(^[ \t]*\n)/gm, "")
 	ydk = ydk.trim()
 	let cardpoolArray = ydk.split("\n")
-	let cardpoolMap = new Map()
-	let convertedYdk = ""
-	cardpoolArray.forEach(convert)
-	cardpoolMap.forEach(logMap)
-	let conf = confHeaders + convertedYdk
-	//const conf = ydk
+	cardpoolArray.forEach(convertListToKeyValuePair)
+	cardpoolMap.forEach(writeKeyValuePairToTextarea)
+	let conf = sneakConfHeaders + convertedYdk
 	conf = conf.trim()
-	document.getElementById("conf").innerHTML = conf
-
-	function convert(currentCard) {
-		if (cardpoolMap.has(currentCard) == true) {
-			if (cardpoolMap.get(currentCard) < 3) {
-				cardpoolMap.set(currentCard, cardpoolMap.get(currentCard) + 1)
-			}
-		} else {
-			cardpoolMap.set(currentCard, 1)
+	document.getElementById("sneakConf").innerHTML = conf
+}
+function bannedListConverterButton() {
+	let googleSpreadsheetsBannedList = document.getElementById("googleSpreadsheetsBannedList").value
+	googleSpreadsheetsBannedList = googleSpreadsheetsBannedList.replace(/passcode	custom	name\n/gi, "")
+	googleSpreadsheetsBannedList = googleSpreadsheetsBannedList.replace(/	/gi, " ")
+	let conf = preXYZConfHeaders + googleSpreadsheetsBannedList
+	conf = conf.trim()
+	document.getElementById("bannedListConf").innerHTML = conf
+}
+function convertListToKeyValuePair(currentCard) {
+	if (cardpoolMap.has(currentCard) == true) {
+		if (cardpoolMap.get(currentCard) < 3) {
+			cardpoolMap.set(currentCard, cardpoolMap.get(currentCard) + 1)
 		}
+	} else {
+		cardpoolMap.set(currentCard, 1)
 	}
-
-	function logMap(value, key) {
-		convertedYdk += key + " " + value + "\n"
-	}
+}
+function writeKeyValuePairToTextarea(value, key) {
+	convertedYdk += key + " " + value + "\n"
 }
