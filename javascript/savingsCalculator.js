6ac907751a5da15d0668f97de5d101d8f55b84f6
GreyGooseVX <total.egal1234@web.de>
2021-07-15 22:59:23 +0200

    .

--- javascript/savingsCalculator.js
+++ javascript/savingsCalculator.js
@@ -6,9 +6,9 @@ document.getElementById("calculateOnSubmit").onsubmit = (event) => {
 function calculateOnSubmit() {
 	const startAmount = Number(document.getElementById("startAmount").value)
 	const monthlySavings = Number(document.getElementById("monthlySavings").value)
-	const effectiveYearlyInterest =
-		Number(document.getElementById("yearlyInterest").value) - Number(document.getElementById("yearlyInflation").value)
-	const monthlyInterestMultipler = Math.pow(effectiveYearlyInterest + 1, 1 / 12)
+	const effectiveYearlyReturns =
+		Number(document.getElementById("yearlyReturns").value) - Number(document.getElementById("yearlyInflation").value)
+	const monthlyReturnsMultipler = Math.pow(effectiveYearlyReturns + 1, 1 / 12)
 	const yearsToBeConsidered = Number(document.getElementById("yearsToBeConsidered").value)
 	const monthsToBeConsidered = yearsToBeConsidered * 12
 	const outputInterval = Number(document.getElementById("outputInterval").value)
@@ -17,11 +17,11 @@ function calculateOnSubmit() {
 		constructor(currentMonth) {
 			if (currentMonth > 0) {
 				this.savingsAfterXMonths =
-					(months[currentMonth - 1].savingsAfterXMonths + monthlySavings) * monthlyInterestMultipler
+					(months[currentMonth - 1].savingsAfterXMonths + monthlySavings) * monthlyReturnsMultipler
 			} else {
 				this.savingsAfterXMonths = startAmount
 			}
-			this.monthlyInterestAfterXMonths = this.savingsAfterXMonths * (monthlyInterestMultipler - 1)
+			this.monthlyReturnsAfterXMonths = this.savingsAfterXMonths * (monthlyReturnsMultipler - 1)
 		}
 	}
 
@@ -33,13 +33,13 @@ function calculateOnSubmit() {
 	results.innerHTML = ""
 	let newLi = document.createElement("div")
 	newLi.className += "list-group-item list-group-item-action"
-	newLi.innerHTML = `effective yearly interest : ${effectiveYearlyInterest}`
+	newLi.innerHTML = `effective yearly returns : ${effectiveYearlyReturns}`
 	results.appendChild(newLi)
 	for (let i = 0; i <= monthsToBeConsidered; i += outputInterval) {
 		let newLi = document.createElement("div")
 		newLi.className += "list-group-item list-group-item-action"
 		newLi.innerHTML = `savings after ${i / 12} year(s) : ${Math.round(months[i].savingsAfterXMonths)}
-		<br> monthly interest after ${i / 12} year(s) : ${Math.round(months[i].monthlyInterestAfterXMonths)}`
+		<br> monthly returns after ${i / 12} year(s) : ${Math.round(months[i].monthlyReturnsAfterXMonths)}`
 		results.appendChild(newLi)
 	}
 }
